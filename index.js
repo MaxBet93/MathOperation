b182a3d3029889923d07b56051c213625aa056c6
MaxBet93 <114855423+MaxBet93@users.noreply.github.com>
2023-09-01 20:40:09 +0300

    Result

---
+++ index.js
@@ -0,0 +1,12 @@
+const firstNumber = prompt("Enter your first number")
+const secondNumber = prompt('Enter your second number')
+
+  let firstOperation = Number(firstNumber)+Number(secondNumber)
+  let secondOperation = Number(firstNumber - secondNumber)
+  let thirdOperation = Number(firstNumber * secondNumber)
+  let fourthOperation = Number(firstNumber / secondNumber)
+  
+alert(`${firstNumber}+${secondNumber}=${firstOperation}
+    ${firstNumber}-${secondNumber}=${secondOperation}
+    ${firstNumber}*${secondNumber}=${thirdOperation}
+    ${firstNumber}/${secondNumber}=${fourthOperation}`)
